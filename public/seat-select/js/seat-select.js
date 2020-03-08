2969fe4f885aff9b9abfd1eab2a70f7affbc3879
Manny Dheer <mannydheer@gmail.com>
2020-03-08 12:06:22 -0400

    Done

--- public/seat-select/js/seat-select.js
+++ public/seat-select/js/seat-select.js
@@ -1,11 +1,19 @@
-const flightInput = document.getElementById('flight');
+// const flightInput = document.getElementById('flight');
 const seatsDiv = document.getElementById('seats-section');
 const confirmButton = document.getElementById('confirm-button');
+//all confirmation values
+const givenName = document.getElementById('givenName');
+const givensurName = document.getElementById('surname');
+const givenEmail = document.getElementById('email');
+const seatNum = document.getElementById('seat-number');
+
 
 let selection = '';
 
-const renderSeats = () => {
+
+const renderSeats = (arrayFlights) => {
     document.querySelector('.form-container').style.display = 'block';
+    console.log(document.querySelector('.form-container'))
 
     const alpha = ['A', 'B', 'C', 'D', 'E', 'F'];
     for (let r = 1; r < 11; r++) {
@@ -16,13 +24,21 @@ const renderSeats = () => {
         for (let s = 1; s < 7; s++) {
             const seatNumber = `${r}${alpha[s-1]}`;
             const seat = document.createElement('li')
-            const seatOccupied = `<li><label class="seat"><span id="${seatNumber}" class="occupied">${seatNumber}</span></label></li>`
-            const seatAvailable = `<li><label class="seat"><input type="radio" name="seat" value="${seatNumber}" /><span id="${seatNumber}" class="avail">${seatNumber}</span></label></li>`        
-            seat.innerHTML = seatAvailable;
+            const seatOccupied = `<li><label class="seat"><span id="${seatNumber}" class="occupied">${seatNumber}</span></label></li>`;
+            const seatAvailable = `<li><label class="seat"><input type="radio" name="seat" value="${seatNumber}" /><span id="${seatNumber}" class="avail">${seatNumber}</span></label></li>`;
+
+            const realSeat = arrayFlights.find(seat => seat.id === seatNumber);
+            // seat.innerHTML = realSeat.isAvailable ? seatAvailable : seatOccupied;
+            //if true, then get Seat Available else seat occupied. 
+            if (realSeat.isAvailable) {
+                seat.innerHTML = seatAvailable
+            } else {
+                seat.innerHTML = seatOccupied
+            }
             row.appendChild(seat);
         }
     }
-    
+
     let seatMap = document.forms['seats'].elements['seat'];
     seatMap.forEach(seat => {
         seat.onclick = () => {
@@ -38,23 +54,144 @@ const renderSeats = () => {
         }
     });
 }
+// -------------------
+var flightKeys = [];
+
+fetch('/allflights', {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        },
+    })
+    .then(array => {
+        return (array.json())
+    })
+    .then(flightArray => {
+        //add event listener on the select tag.
+        let selectTag = document.getElementById('dropDown')
+        selectTag.addEventListener('change', passEvent);
+
+        flightArray.forEach(flight => {
+            flightKeys.push(flight)
+            let option = document.createElement('option')
+            option.id = `${flight}`;
+            document.getElementById('dropDown').appendChild(option);
+            option.innerText = `${flight}`;
+
+        });
+    })
+// -------------------
 
 
+//let 
+const handlefindReservation = (event) => {
+    event.preventDefault();
+
+    let emailInput = document.getElementById('emailFinder');
+    let emailValue = emailInput.value; //send as string?
+    // let emailEntered = JSON.stringify(emailValue)
+
+    fetch(`/getReservation/${emailValue}`, {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            }
+        })
+        .then(userInfo => {
+            return (userInfo.json());
+        })
+        .then(newData => {
+
+            window.location.href = `/seat-select/view-reservation.html?givenName=${newData.givenName}&surName=${newData.surname}&newEmail=${newData.email}&flightNumber=${newData.flight}&idNumber=${newData.id}`
+
+
+        })
+
+}
+
+function passEvent(event) {
+    toggleFormContent(event)
+}
+//declare globally so you can pass to hangleConfirmSeat
+let flightValue;
 const toggleFormContent = (event) => {
-    const flightNumber = flightInput.value;
-    console.log('toggleFormContent: ', flightNumber);
-    
-    // TODO: contact the server to get the seating availability
-    //      - only contact the server if the flight number is this format 'SA###'.
-    //      - Do I need to create an error message if the number is not valid?
-    
-    // TODO: Pass the response data to renderSeats to create the appropriate seat-type.
-    renderSeats();
+    //get flight value number entered.
+    flightValue = event.target.value
+
+    fetch(`/seat-select/${flightValue}`, {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            },
+        })
+        .then(arrayFlights => {
+            return (arrayFlights.json())
+        })
+        // TODO: Pass the response data to renderSeats to create the appropriate seat-type.
+        .then(arrayFlights => {
+            //the chosen flights data (array)
+            renderSeats(arrayFlights);
+        })
+
 }
 
-const handleConfirmSeat = () => {
+const handleConfirmSeat = (event) => {
+    event.preventDefault();
+    let seatBrackets = seatNum.innerText;
+    let removeBrackets = seatBrackets.replace(')', "");
+    let seatValue = removeBrackets.replace('(', "");
+
     // TODO: everything in here!
+    const data = {
+        //remove brackets from seatNum
+        flight: flightValue,
+        seat: seatValue,
+        givenName: givensurName.value,
+        surname: givensurName.value,
+        email: givenEmail.value,
 
+    };
+
+
+    fetch(`/confirmation`, {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+
+        })
+        .then(data => {
+            return (data.json())
+
+        })
+        .then(info => {
+            console.log(info)
+            let newInfo = JSON.stringify(info)
+            console.log(info)
+            window.location.href = `/seat-select/confirmed.html?name=${info.givenName}&surname=${info.surname}&email=${info.email}&seat=${info.seat}&flight=${info.flight}`
+
+
+        })
 }
 
-flightInput.addEventListener('blur', toggleFormContent);
+///ADMIN
+
+const handleAdmin = (event) => {
+    event.preventDefault();
+
+    let getadminEle = document.getElementById('admin');
+    let adminLogin = getadminEle.value;
+
+    if (adminLogin === 'admin') {
+        window.location.href = `/seat-select/admin.html`;
+
+
+
+    }
+
+}
--- server.js
+++ server.js
@@ -1,23 +1,105 @@
 'use strict';
+const request = require('request-promise');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const {
+    flights,
+    eachFlight,
+    eachReservation,
+    eachEmail,
+    adminReservations
+} = require('./test-data/flightSeating')
 
 const PORT = process.env.PORT || 8000;
 
+const handleConfirmation = async (req, res) => {
+    const data = req.body;
+    let reservation = await eachReservation(data)
+    console.log(reservation.reservation)
+
+    if (reservation.status === 201) {
+        res.send(reservation.reservation)
+
+    } else {
+        console.log('ERROR');
+    }
+}
+
+const handleallFlights = async (req, res) => {
+
+    let flightsAll = await flights();
+    let onlyFlights = flightsAll.flights
+
+    res.send(onlyFlights)
+}
+
+const handleEachFlight = async (req, res) => {
+
+    let currentFlightNumber = req.params.flightNum
+    let eachFlightArray = await eachFlight(currentFlightNumber);
+    //
+    let flightKey = (Object.keys(eachFlightArray))
+    let getflight = eachFlightArray[flightKey]
+
+    res.send(getflight)
+
+}
+const handlegetReservation = async (req, res) => {
+
+    let currentEmail = req.params.email
+    let receivedEmails = await eachEmail(currentEmail);
+    let userReservation = receivedEmails.data
+    res.send(userReservation)
+
+}
+const handleReservationsForSelectedFlight = async (req,res) => {
+    let getFlightInput = req.params.flightInput;
+
+    let allreqReservations = await adminReservations();
+    
+    let storedFlights = [];
+
+    allreqReservations.forEach(reservation => {
+        if (reservation.flight == getFlightInput) {
+            storedFlights.push(reservation)
+        }
+
+        
+    });
+    
+    res.send(storedFlights)
+
+
+
+
+}
+
+
+
+
+
 express()
-    .use(function(req, res, next) {
+    .use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     })
-	.use(morgan('dev'))
-	.use(express.static('public'))
+    .use(morgan('dev'))
+    .use(express.static('public'))
     .use(bodyParser.json())
-    .use(express.urlencoded({extended: false}))
-    
+    .use(express.urlencoded({
+        extended: false
+    }))
+    .set('view engine', 'ejs')
+
     // endpoints
+    .get('/seat-select/:flightNum', handleEachFlight)
+    .post('/confirmation', handleConfirmation)
+    .get('/allflights', handleallFlights)
+    .get('/getReservation/:email', handlegetReservation)
+    .get('/getselectedFlight/:flightInput', handleReservationsForSelectedFlight)
 
     .use((req, res) => res.send('Not Found'))
     .listen(PORT, () => console.log(`Listening on port ${PORT}`));
